dadfbefb9b9676641628ffb06a9472dc8c063596
AshishKohad <kohadashish27@gmail.com>
2022-07-23 01:37:02 +0530

    code format

--- Scripts/block_4.js
+++ Scripts/block_4.js
@@ -1,15 +1,15 @@
 let block_4 = [
-    "https://assets.www.happyfox.com/v2/logo/owler-logo.png",
-    "https://assets.www.happyfox.com/v2/logo/whirlpool.svg",
-    "https://assets.www.happyfox.com/v2/logo/jabra.svg",
-    "https://assets.www.happyfox.com/v2/logo/leap-frog.svg",
-    "https://assets.www.happyfox.com/v2/logo/harbour.svg"
-  ]
+  "https://assets.www.happyfox.com/v2/logo/owler-logo.png",
+  "https://assets.www.happyfox.com/v2/logo/whirlpool.svg",
+  "https://assets.www.happyfox.com/v2/logo/jabra.svg",
+  "https://assets.www.happyfox.com/v2/logo/leap-frog.svg",
+  "https://assets.www.happyfox.com/v2/logo/harbour.svg",
+];
 
-  block_4.forEach(function (ele) {
-      let div_main = document.createElement("div");
-      let image = document.createElement("img");
-      image.setAttribute("src",ele);
-      div_main.append(image);
-      document.querySelector("#block_4>div").append(div_main);
-  })
+block_4.forEach(function (ele) {
+  let div_main = document.createElement("div");
+  let image = document.createElement("img");
+  image.setAttribute("src", ele);
+  div_main.append(image);
+  document.querySelector("#block_4>div").append(div_main);
+});
